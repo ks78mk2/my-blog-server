ec603d406bc2316deee652df046ddd84ee68cc94
jskim <ks78mk2@naver.com>
2021-08-30 13:55:31 +0900

    test

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import * as helmet from 'helmet';
 import * as cookieParser from 'cookie-parser';
-const fs = require('fs');
+// const fs = require('fs');
 import { AppModule } from './app.module';
 import { _ValidationPipe } from './commons/exception/validationPipeError';
 import { TransformInterceptor } from './commons/util/response.util';
 
 async function bootstrap() {
-  let httpsOptions = {};
-  if(process.env.SSL == 'true'){ 
-    httpsOptions= {
-      key: fs.readFileSync('/etc/ssl/cert/privkey1.pem'),
-      cert: fs.readFileSync('/etc/ssl/cert/fullchain1.pem'),
-    };
-  }  
+  // let httpsOptions = {};
+  // if(process.env.SSL == 'true'){ 
+  //   httpsOptions= {
+  //     key: fs.readFileSync('/etc/ssl/cert/privkey1.pem'),
+  //     cert: fs.readFileSync('/etc/ssl/cert/fullchain1.pem'),
+  //   };
+  // }  
 
-  const app = await NestFactory.create(AppModule, { ...httpsOptions, cors: true });
+  // const app = await NestFactory.create(AppModule, { ...httpsOptions, cors: true });
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.use(cookieParser());
   app.useGlobalInterceptors(new TransformInterceptor());
   app.setGlobalPrefix('api/v1');
